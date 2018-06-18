aa57fb0be63fb98aec1e00d6f01e704469186c16
thesouldemon <noah.kalinowski@gmail.com>
2018-06-17 19:25:40 -0500

    complete refactor using hqbuff api

--- scrape_questions.py
+++ scrape_questions.py
@@ -1,11 +1,8 @@
+# -*- coding: utf-8 -*-
 import requests
 import codecs
 from bs4 import BeautifulSoup
-
-
-class QuestionException(Exception):
-    def __init__(self, url="Error parsing question"):
-        Exception.__init__(self, "Error on " + url)
+import json
 
 
 class GameError(Exception):
@@ -13,90 +10,44 @@ class GameError(Exception):
         Exception.__init__(self, "Game " + game + " does not exist")
 
 
-def get_title(question):
+def write_one_game(url, file):
     """
-    Gets the title from the question html
-    :param question: the question html 'object'
-    :return: the text of the tile
+    Writes one game to the file
+    :param url: the url of the game we are writing (date format)
+    :param file: the file we are writing to
+    :return:
     """
-
-    items = question.find_all('strong')
-    if len(items) == 0:
-        raise QuestionException
-    title = items[0].get_text().strip()
-    if 'Savage' in title:
-        title = title[:-7].strip()
-    return title
-
-
-def get_answers(question):
-    """
-    Gets a list of all the answers and the correct answer from html
-    :param question: the html question 'object'
-    :return: the answers then the correct answer index in a list
-    """
-    answers = question.find('ul', {'class': 'questions'})
-    if answers is None:
-        raise QuestionException("question missing")
-    answers = answers.find_all('li')
-    all_answers = []
-
-    for answer in answers:
-        answer = answer.get_text()
-        if 'Correct' in answer:
-            answer = answer[:-8]
-        all_answers.append(answer)
-
-    for answer in answers:
-        # the answer has a span, incorrect does not
-        if answer.find('span') is not None:
-            answer_text = answer.get_text()
-            # remove trailing space
-            all_answers.append(answer_text[:-8])
-
-    return all_answers
-
-
-def write_one_game(url):
-    # local testing file
-    # soup = BeautifulSoup(open('hq.html', encoding='utf8'), 'lxml')
+    url = "https://hqbuff.com/api/" + url
 
     headers = {"user-agent": "questionScrape"}
     req = requests.get(url, headers)
     req.encoding = 'utf-8'
-    # soup the url
-    soup = BeautifulSoup(req.text, "html5lib")
 
-    # find all question classes, minus the last (winner data)
-    questions = soup.find_all("div", {"class": "question"})[:-1]
-    if len(questions) == 0:
+    data = json.loads(req.text)
+    if len(data) == 0:
         raise GameError
 
-    # open the file to write to
-    f = codecs.open('questions.txt', 'a')
+    # loop through every game
+    for game in data:
+        # we only care about the question key
+        for question in game['questions']:
+            answer_str = ""
+            correct = ""
+            title = question['text']
+            # loop through all answers, store the correct one
+            for a in question['answers']:
+                answer_str += a['text'] + ","
+                if a['correct']:
+                    correct = a['text']
+            answer_str += correct
 
-    # loop through all the questions
-    for q in questions:
-        # wrap in try except to catch sponsors embedded in qs list
-        try:
-            title = get_title(q)
-            title = title.replace('“', '"')
-            title = title.replace('”', '"')
-            title = title.replace('’', "'")
-            # get the answers
-            answers = get_answers(q)
-            # turn answers list into string
-            answers_str = ",".join(answers)
-            # write the answers
+            # write the final output to the file
             try:
-                f.write(title + '|' + answers_str + '\n')
+                file.write(title + '|' + answer_str + '\n')
+            # this should'nt come up anymore, but just in case there is a strange symbol
             except UnicodeEncodeError:
                 print("error writing question")
                 continue
-        except QuestionException:
-            print(QuestionException("question"))
-
-    f.close()
 
 
 def main():
@@ -105,22 +56,20 @@ def main():
     soup = BeautifulSoup(req.text, 'html5lib')
     # get all the games from hqbuff
     links = soup.find_all("ul", {"class": "list--archive"})[0]
-    all_games = [link.get('href') for link in links.find_all('a')]
+    # slice the game urls to be in YYYY-MM-DD format
+    all_games = [link.get('href')[6:16] for link in links.find_all('a')]
+
+    # open the file outside of the loop so we dont open and close the file many times
+    f = codecs.open('questions.txt', 'w', encoding='utf-8')
+
     # write the data for every question in the games to the file
     for link in all_games:
-        url = 'http://hqbuff.com' + link
         try:
-            write_one_game(url)  # we must prepend the url
+            write_one_game(link, f)  # we must prepend the url
         except GameError:
-            print(GameError(url))
+            print(GameError(link))
 
-        # now for second game
-
-        url = 'http://hqbuff.com' + link[:-1] + "2"
-        try:
-            write_one_game(url)
-        except GameError:
-            print(GameError(url))
+    f.close()
 
 
 if __name__ == main():
